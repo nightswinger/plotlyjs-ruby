323aa503e05f290a376a194cff414c97f8e25e6d
nightswinger <stardustkids83@gmail.com>
2024-12-02 02:45:02 +0000

    move Chart class to a separate file

--- lib/plotlyjs-ruby.rb
+++ lib/plotlyjs-ruby.rb
@@ -1,5 +1,6 @@
 require 'json'
 
+require_relative 'plotlyjs/chart'
 require_relative 'plotlyjs/version'
 
 module Plotlyjs
@@ -7,45 +8,6 @@ module Plotlyjs
     def new_plot(data, layout = {}, config = {})
       chart = Chart.new(data, layout, config)
       chart.to_html
-    end
-  end
-
-  class Chart
-    attr_reader :data, :layout, :config
-
-    def initialize(data, layout, config)
-      @data = data
-      @layout = layout
-      @config = config
-    end
-
-    def to_html
-      element_id = "plotly-chart-#{self.object_id}"
-
-      js_vars = {
-        data: data.to_json,
-        layout: layout.to_json,
-        config: config.to_json
-      }
-
-      createjs = "Plotly.newPlot('#{element_id}', %{data}, %{layout}, %{config});" % js_vars
-
-      html = <<~HTML
-        <div id="#{element_id}"></div>
-        <script>
-          (function() {
-            var createChart = function() { #{createjs} };
-
-            if ("Plotly" in window) {
-              createChart();
-            } else {
-              window.addEventListener('load', createChart);
-            };
-          })();
-        </script>
-      HTML
-
-      html.respond_to?(:html_safe) ? html.html_safe : html
     end
   end
 end
---
+++ lib/plotlyjs/chart.rb
@@ -0,0 +1,40 @@
+module Plotlyjs
+  class Chart
+    attr_reader :data, :layout, :config
+
+    def initialize(data, layout, config)
+      @data = data
+      @layout = layout
+      @config = config
+    end
+
+    def to_html
+      element_id = "plotly-chart-#{self.object_id}"
+
+      js_vars = {
+        data: data.to_json,
+        layout: layout.to_json,
+        config: config.to_json
+      }
+
+      createjs = "Plotly.newPlot('#{element_id}', %{data}, %{layout}, %{config});" % js_vars
+
+      html = <<~HTML
+        <div id="#{element_id}"></div>
+        <script>
+          (function() {
+            var createChart = function() { #{createjs} };
+
+            if ("Plotly" in window) {
+              createChart();
+            } else {
+              window.addEventListener('load', createChart);
+            };
+          })();
+        </script>
+      HTML
+
+      html.respond_to?(:html_safe) ? html.html_safe : html
+    end
+  end
+end
